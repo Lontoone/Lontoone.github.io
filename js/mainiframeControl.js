a4db513fe816acaba4bf6547a06a8705205dc1fe
Lontoone <news2000tw@gmail.com>
2021-02-26 13:57:56 +0800

    gamesPage

--- js/mainiframeControl.js
+++ js/mainiframeControl.js
@@ -1,13 +1,14 @@
 $(document).on("click", "#nav-home", function () {
-  console.log("hi");
   document.getElementById("iMiddleContent").src = "../html/home.html";
 });
 
 $(document).on("click", "#nav-blog", function () {
-    console.log("hi");
-    document.getElementById("iMiddleContent").src = "../html/blog.html";
-  });
-  
+  document.getElementById("iMiddleContent").src = "../html/blog.html";
+});
+
+$(document).on("click", "#nav-games", function () {
+  document.getElementById("iMiddleContent").src = "../html/games.html";
+});
 
 /*
 function setIframe(page) {
