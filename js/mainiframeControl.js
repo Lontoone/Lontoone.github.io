54e38268038e4eebabcd72597f3d2ed4febeaae7
Lontoone <news2000tw@gmail.com>
2021-03-13 03:18:10 +0800

    geo 補上字型

--- js/mainiframeControl.js
+++ js/mainiframeControl.js
@@ -9,7 +9,12 @@ $(document).on("click", "#nav-blog", function () {
 $(document).on("click", "#nav-games", function () {
   document.getElementById("iMiddleContent").src = "../html/games.html";
 });
-
+$(document).on("click", "#nav-webs", function () {
+  document.getElementById("iMiddleContent").src = "../html/website.html";
+});
+$(document).on("click", "#nav-git", function () {
+  document.getElementById("iMiddleContent").src = "../html/github.html";
+});
 /*
 function setIframe(page) {
   $("#iMiddleContent").src = "../html/" + page + ".html";
