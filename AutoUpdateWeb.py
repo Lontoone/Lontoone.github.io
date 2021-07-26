8bb9199b40d1e5dfec524066fc656dcd5a756708
cingjia <news2000tw@gmail.com>
2021-07-26 17:42:25 +0900

    baha data Update

--- AutoUpdateWeb.py
+++ AutoUpdateWeb.py
@@ -4,15 +4,5 @@ subprocess.run(["python","test.py"], cwd="D:\MyGit\Lontoone.github.io\data").std
 subprocess.run(["git","add","."], cwd="D:\MyGit\Lontoone.github.io")
 subprocess.run(["git","commit","-m","baha data Update"], cwd="D:\MyGit\Lontoone.github.io")
 subprocess.run(["git","push"], cwd="D:\MyGit\Lontoone.github.io")
-subprocess.run(["pause"])
 
-'''subprocess.run(["git","push","origin","main"], cwd="D:\MyGit\Lontoone.github.io");'''
-
-
-'''os.system('cd /d D:\MyGit\Lontoone.github.io\data')
-os.system('python test.py')'''
-'''
-os.system('cd ../')
-os.system('git commit -m "baha data update"')
-os.system('git push origin main')
-'''
+'''subprocess.run(["pause"])'''
